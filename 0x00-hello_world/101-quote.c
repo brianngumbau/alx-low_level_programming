838c0f6f6d6b558fca84eb0f4c9a10e75a651e4e
brianngumbau <brianngumbau9075@gmail.com>
2023-07-13 12:54:53 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,10 @@
 #include <unistd.h>
 /**
  * main - A program that prints a line to the standard error
- * Return:1 (success)
+ * Return 1 (success)
  */
 int main(void)
 {
-	gets("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
+}
