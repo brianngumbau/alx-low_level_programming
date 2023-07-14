4172be0efe4b0e49c61bb750f7bd74daf7fdf72c
brianngumbau <brianngumbau9075@gmail.com>
2023-07-14 04:24:48 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - a program that prints lowercase alphabets except q and e
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'q' && 'e')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+	putchar ('\n');
+	return (0);
+}
+
