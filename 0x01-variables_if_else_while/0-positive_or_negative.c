a1659548d12c9491fe048f83ac515cc452f85f5a
brianngumbau <brianngumbau9075@gmail.com>
2023-07-14 02:06:53 -0700

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,17 +1,19 @@
 #include <stdlib.h>
 #include <time.h>
 #include<stdio.h>
+
 /**
- * main - A program that will assign a ramdom number to the variable n
- * Return 0 (success)
+ * main - main program
+ * Return : always 0
  */
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0)
+	if (n>0)
 		printf("%d is positive\n", n);
 	else if (n==0)
 		printf("%d is zero\n", n);
