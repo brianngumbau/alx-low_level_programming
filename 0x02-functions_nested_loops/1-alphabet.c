f87c5e45c8b0d20c7b98143ee42cd465540becf8
brianngumbau <brianngumbau9075@gmail.com>
2023-07-18 01:56:54 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - a function that prints a-z
+ * Return:void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for(c = 'a'; c<= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
