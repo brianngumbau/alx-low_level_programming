297b8b39ada613a586ec3276cf6bf79753aa8712
brianngumbau <brianngumbau9075@gmail.com>
2023-07-18 08:31:13 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,29 +10,29 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-	for (b = 0; b <= 9; b++)
-	{
-	x = a * b;
-	if (x > 9)
-	{
-	y = x % 10;
-	z = (x - y) / 10;
-	_putchar(44);
-	_putchar(32);
-	_putchar(z + '0');
-	_putchar(y + '0');
-	}
-	else
-	{
-		if (b != 0)
-	{
-	_putchar(44);
-	_putchar(32);
-	_putchar(32);
-	}
-	_putchar(x + '0');
-	}
-	}
-	_putchar('\n');
+		for (b = 0; b <= 9; b++)
+		{
+			x = a * b;
+			if (x > 9)
+			{
+				y = x % 10;
+				z = (x - y) / 10;
+				_putchar(44);
+				_putchar(32);
+				_putchar(z + '0');
+				_putchar(y + '0');
+			}
+			else
+			{
+				if (b != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(x + '0');
+			}
+		}
+		_putchar('\n');
 	}
 }
