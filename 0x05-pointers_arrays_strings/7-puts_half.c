9caa0358e40779be4ff02cbba4c7f20a17f11e29
brianngumbau <brianngumbau9075@gmail.com>
2023-07-24 04:26:27 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: the string to print
+ * Return: empty
+ */
+void puts_half(char *str)
+{
+	int l = 0;
+	int m;
+
+	while (str[l] != '\0')
+	{
+		l++;
+	}
+	if (l % 2 == 0)
+	{
+		m = (l - 1) / 2;
+		m += 1;
+	}
+	else
+	{
+		m = l / 2;
+	}
+}
