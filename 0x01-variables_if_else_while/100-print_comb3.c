409017874d833a8e26bb4da84d80402b4f2a89f2
brianngumbau <brianngumbau9075@gmail.com>
2023-08-01 00:53:30 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,17 +5,17 @@
  */
 int main(void)
 {
-	int i = '0';
-	int j = '1';
+	int i;
+	int j;
 
 	for (i = 0; i < 9; i++)
 	{
-		for (j = 1; j <= 9; j++)
+		for (j = i + 1; j <= 9; j++)
 		{
 			if (!(i > j) || !(i == j))
 			{
-				putchar(i);
-				putchar(j);
+				putchar('i');
+				putchar('j');
 				if (i == '8' && j == '9')
 				{
 					putchar('\n');
