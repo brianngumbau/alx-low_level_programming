0b31391b1648e6aa9f167b0da58577ddb1a6f18b
brianngumbau <brianngumbau9075@gmail.com>
2023-08-07 04:32:06 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,12 +16,12 @@ char *_strdup(char *str)
 	x = 0;
 	while (str[x] != '\0')
 		x++;
-	
+
 	s = malloc(sizeof(char) * (x + 1));
 
 	if (s == NULL)
 		return (NULL);
-	
+
 	for (y = 0; str[y]; y++)
 		s[y] = str[y];
 	return (s);
