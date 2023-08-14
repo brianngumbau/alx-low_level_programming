684ec6f04e19e11ec65dae53b98fcbb68e48eb76
brianngumbau <brianngumbau9075@gmail.com>
2023-08-14 07:14:04 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,7 +3,7 @@
 
 /**
  * init_dog - initializes a variable of type dog
- * @d: pointer to struct dog 
+ * @d: pointer to struct dog
  * @name: name to initialize
  * @age: age to initialize
  * @owner: owner to initialize
