6aec75fc774556f1cd8a926f38ff7abeb27504ee
brianngumbau <brianngumbau@gmail.com>
2023-08-25 02:40:45 -0700

    sinly linked list

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,20 @@
-	global  main
-	  extern   printf
-main:
-	  mov  edi, format
-	  xor  eax, eax
-	  call printf
-	  mov  eax, 0
-	  ret
-format: db 'Hello, Holberton\n' ,0
+ section .data
+    hello db "Hello, Holberton",0
+    format db "%s", 0
+    newline db 10,0
+
+ section .text
+     extern printf
+
+
+ global main
+ main:
+     push rbp
+     mov rdi, format
+     mov rsi, hello
+     call printf
+     mov rdi, format
+     mov rsi, newline
+     call printf
+     pop rbp
+     ret
