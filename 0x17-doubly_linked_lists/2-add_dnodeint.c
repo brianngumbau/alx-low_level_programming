f595df4915c73de2c658b1ba699637538f987f9e
brianngumbau <brianngumbau9075@gmail.com>
2023-10-12 08:28:48 -0700

    commit

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,37 @@
+#include "lists"
+
+/**
+ * add_dnodeint - adds new node(beginning)
+ * @head: head of list
+ * @n: value
+ * Return: address of new element
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+	dlistint_t *b;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = NULL;
+	b = *head;
+
+	if (b != NULL)
+	{
+		while (b->prev != NULL)
+			b = b->prev;
+	}
+
+	new->next = b;
+
+	if (b != NULL)
+		b->prev = new;
+
+	*head = new;
+
+	return (new);
+}
